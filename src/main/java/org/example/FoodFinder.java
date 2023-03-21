ec57a4e804f8f5edfae705923861306fae7fbdd2
BuildTools <unconfigured@null.spigotmc.org>
2023-03-21 19:13:12 +0100

    added FoodFinder with simple recipe jajecznica

---
+++ src/main/java/org/example/FoodFinder.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.List;
+
+public class FoodFinder {
+    Recipe createRecipe(List<String> ingredients) {
+        Recipe recipe = new Recipe("jajecznica");
+
+        return recipe;
+
+    }
+}
---
+++ src/main/java/org/example/Recipe.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public record Recipe(String name){
+
+}
